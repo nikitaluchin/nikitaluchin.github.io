29c28530636fc3d569080b2e8c46a481c788ce66
nikitaluchin <luchinnightwolf543@gmail.com>
2022-10-02 01:18:26 +0300

    v7.3

--- task5/assets/main.js
+++ task5/assets/main.js
@@ -1,3 +1,5 @@
+const MY_COLOR = "#1E4AD6";
+
 function onClick() {
     let amount = document.getElementsByName("amount"); // NodeList
     let cost = document.getElementsByName("cost");
@@ -9,13 +11,16 @@ function onClick() {
 
     // === - так безопаснее проверять: без автопреобразования типов
     if (amount[0].value === "" || cost[0].value === "") {
+        result.style.color = "red";
         result.innerHTML = "Можно не надо так делать...";
     } else if ((checkAmount || checkCost) === null) {
+        result.style.color = MY_COLOR;
         let intAmount = parseInt(amount[0].value);
         let intCost = parseInt(cost[0].value);
         result.innerHTML = "Я вам насчитал тут: " + intAmount * intCost;
     } else {
         // сработает при "123@", например
+        result.style.color = "red";
         result.innerHTML = "Ну зачем вы так :(";
     }
 }
