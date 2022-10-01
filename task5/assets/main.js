762e602bb31b8c7eb67302c77e82c16d83ce29b4
nikitaluchin <luchinnightwolf543@gmail.com>
2022-10-02 00:36:46 +0300

    v7.2

--- task5/assets/main.js
+++ task5/assets/main.js
@@ -2,13 +2,15 @@ function onClick() {
     let amount = document.getElementsByName("amount"); // NodeList
     let cost = document.getElementsByName("cost");
     let result = document.getElementById("result");
+
     const re = /\D/; // \D - нашло НЕ цифру. ну а шаблоны для regex пишутся в /.../
     let checkAmount = amount[0].value.match(re);
     let checkCost = cost[0].value.match(re);
-    if (parseInt(amount[0].value).isNaN() || parseInt(cost[0].value).isNaN()) {
+
+    // === - так безопаснее проверять: без автопреобразования типов
+    if (amount[0].value === "" || cost[0].value === "") {
         result.innerHTML = "Можно не надо так делать...";
     } else if ((checkAmount || checkCost) === null) {
-        // === - так безопаснее проверять: без автопреобразования типов
         let intAmount = parseInt(amount[0].value);
         let intCost = parseInt(cost[0].value);
         result.innerHTML = "Я вам насчитал тут: " + intAmount * intCost;
